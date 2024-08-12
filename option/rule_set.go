66b06fba43a6b9bd612b440f966e149da1f0d187
PuerNya <safarier@outlook.com>
2024-08-13 06:42:47 +0800

    add rule-provider clash-api
    Signed-off-by: lux5am <lux.5am@yandex.com>

--- option/rule_set.go
+++ option/rule_set.go
@@ -69,7 +69,7 @@ func (r *RuleSet) UnmarshalJSON(bytes []byte) error {
 			return E.New("unknown rule-set format: " + r.Format)
 		}
 	} else {
-		r.Format = ""
+		r.Format = C.RuleSetFormatSource
 		r.Path = ""
 	}
 	err = UnmarshallExcluded(bytes, (*_RuleSet)(r), v)
